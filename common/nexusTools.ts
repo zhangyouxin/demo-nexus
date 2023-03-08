1f9358d29492a7f145d7f08bcd6d3b75e348f57e
zhangyouxin <zhangyouxingg@126.com>
2023-03-08 21:59:49 +0800

    refactor: extract default_nexus_page_size

--- common/nexusTools.ts
+++ common/nexusTools.ts
@@ -1,4 +1,7 @@
 import { Cell, Script } from "@ckb-lumos/lumos";
+
+export const DEFAULT_NEXUS_PAGE_SIZE = 20;
+
 export const getOffChainLocks = async (ckb: any) => {
   return await ckb.fullOwnership.getOffChainLocks({});
 };
@@ -11,8 +14,8 @@ export const getOnChainLocks = async (
   // get first page
   let onChainInfos = await ckb.fullOwnership.getOnChainLocks({ change: type });
   locks.push(...onChainInfos.objects);
-  // if the first page has 20 items, then there are more pages, nexus default page size is 20
-  while (onChainInfos.objects.length === 20) {
+  // if the first page has DEFAULT_NEXUS_PAGE_SIZE items, then there are more pages, nexus default page size is DEFAULT_NEXUS_PAGE_SIZE
+  while (onChainInfos.objects.length === DEFAULT_NEXUS_PAGE_SIZE) {
     onChainInfos = await ckb.fullOwnership.getOnChainLocks({
       cursor: onChainInfos.cursor,
       change: type,
@@ -27,8 +30,8 @@ export const getAllLiveCells = async (ckb: any): Promise<Cell[]> => {
   // get first page of live cells
   let liveCellsResult = await ckb.fullOwnership.getLiveCells({});
   fullCells.push(...liveCellsResult.objects);
-  // if the first page has 20 items, then there are more pages, nexus default page size is 20
-  while (liveCellsResult.objects.length === 20) {
+  // if the first page has DEFAULT_NEXUS_PAGE_SIZE items, then there are more pages, nexus default page size is DEFAULT_NEXUS_PAGE_SIZE
+  while (liveCellsResult.objects.length === DEFAULT_NEXUS_PAGE_SIZE) {
     liveCellsResult = await ckb.fullOwnership.getLiveCells({
       cursor: liveCellsResult.cursor,
     });
